6a34a789162af69e3b78f3dc914c7580e6a3d332
Nikita Bulavin <nikbulavin@proton.me>
2025-07-28 12:02:21 +0300

    Update dependencies

--- build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
@@ -1,30 +1,33 @@
 import com.android.build.gradle.LibraryExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
 import ru.resodostudio.flick.libs
 
 class AndroidFeatureConventionPlugin : Plugin<Project> {
+
     override fun apply(target: Project) {
         with(target) {
-            pluginManager.apply {
-                apply("flick.android.library")
-                apply("flick.hilt")
-            }
+            apply(plugin = "flick.android.library")
+            apply(plugin = "flick.hilt")
+            apply(plugin = "org.jetbrains.kotlin.plugin.serialization")
+
             extensions.configure<LibraryExtension> {
                 testOptions.animationsDisabled = true
             }
 
             dependencies {
-                add("implementation", project(":core:designsystem"))
-                add("implementation", project(":core:ui"))
+                "implementation"(project(":core:ui"))
+                "implementation"(project(":core:designsystem"))
 
-                add("implementation", libs.findLibrary("androidx.hilt.navigation.compose").get())
-                add("implementation", libs.findLibrary("androidx.lifecycle.runtimeCompose").get())
-                add("implementation", libs.findLibrary("androidx.lifecycle.viewModelCompose").get())
-
-                add("implementation", libs.findLibrary("kotlinx.coroutines.android").get())
+                "implementation"(libs.findLibrary("androidx.hilt.navigation.compose").get())
+                "implementation"(libs.findLibrary("androidx.lifecycle.runtimeCompose").get())
+                "implementation"(libs.findLibrary("androidx.lifecycle.viewModelCompose").get())
+                "implementation"(libs.findLibrary("androidx.navigation.compose").get())
+                "implementation"(libs.findLibrary("androidx.tracing").get())
+                "implementation"(libs.findLibrary("kotlinx.serialization.json").get())
             }
         }
     }
